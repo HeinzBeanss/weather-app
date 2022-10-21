2c93deeb00f18b79c05a5dbf5e94e87565be42fc
David Bean <heinzbeansss@gmail.com>
2022-10-21 23:34:03 +0100

    Create functioning current weather app

--- main.js
+++ main.js
@@ -14,18 +14,104 @@ function setTheme() {
 document.querySelector(".theme-toggle").addEventListener("click", setTheme);
 
 // -------------------
-// api key: aa73dae379feac51e8ebefcfd9b41a45
 
-const p = document.querySelector("p");
 const searchbar = document.querySelector(".searchbar");
+
 const searchbutton = document.querySelector(".searchbutton");
 const error = document.querySelector(".error");
+const image = document.querySelector(".image");
+const celcius = document.querySelector(".C");
+const fahrenheit = document.querySelector(".F");
+
+const nametitle = document.querySelector(".nametitle");
+const country = document.getElementById("country");
+const sunrise = document.getElementById("sunrise");
+const sunset = document.getElementById("sunset");
+
+const main = document.getElementById("main");
+const desc = document.getElementById("desc");
+const temp = document.getElementById("temp");
+const humidity = document.getElementById("humidity");
+const visibility = document.getElementById("visibility");
+let answer = "";
+let temperatureCel; 
+let temperatureFah;
+
+function convertTime(unixTime) {
+  const offset = new Date().getTimezoneOffset();
+  console.log(offset);
+  const userTimezoneDifference = offset * 60;
+  const newTime = new Date((unixTime + userTimezoneDifference) * 1000);
+  // get the actual time set in stone. then we change it to that area's timezone difference, then we take the users time difference from UTC as well, otherwise for me for instance, it would be in UTC and my clock's aren't in UTC.  
+  // const offset = newTime.getTimezoneOffset();
+
+  // const offsethours = offset / 60;
+  // console.log(offsethours); // UTC is (-)1 hour behind me.
+  const minutes = newTime.getMinutes().toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false});
+  const hours = newTime.getHours().toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false});
+  answer = `${hours}:${minutes}`;
+  return answer;
+}
+
+function displayMainWeather(weatherInfo) {
+
+  function convertToCec(tempKelvin) {
+    temperatureCel = tempKelvin - 273.15;
+    temperatureCel = Math.round(temperatureCel);
+    return temperatureCel;
+  }
+
+  function convertToFah(tempKelvin) {
+    temperatureFah = ((tempKelvin - 273.15) * 1.8) + 32;
+    temperatureFah = Math.round(temperatureFah);
+    return temperatureFah;
+  }
+
+  console.log(weatherInfo);
+  nametitle.textContent = weatherInfo.name;
+  country.textContent = weatherInfo.sys.country;
+  console.log(weatherInfo.sys.sunrise);
+  console.log(weatherInfo.sys.sunrise + weatherInfo.timezone);
+  convertTime(weatherInfo.sys.sunrise + weatherInfo.timezone);
+  sunrise.textContent = answer;
+  convertTime(weatherInfo.sys.sunset + weatherInfo.timezone);
+  sunset.textContent = answer;
+  main.textContent = weatherInfo.weather[0].main;
+  image.style.backgroundImage = `url("assets/${weatherInfo.weather[0].main}.jpg")`;
+  
+  desc.textContent = weatherInfo.weather[0].description;
+  convertToCec(weatherInfo.main.temp);
+  convertToFah(weatherInfo.main.temp);
+
+  if (celcius.className === "C selected") {
+    console.log("celcius done this")
+    temp.textContent = `${temperatureCel}°C`;
+  } else if (fahrenheit.className === "F selected") {
+    console.log("fahrenheit done this");
+    temp.textContent = `${temperatureFah}°F`;
+  }
+  
+  humidity.textContent = `${weatherInfo.main.humidity}%`;
+  visibility.textContent = weatherInfo.visibility;
+
+}
+
+function displayCel() {
+  // testTemp = temp.textContent;
+  console.log(temperatureCel);
+  temp.textContent = `${temperatureCel}°C`;
+  celcius.classList.add("selected");
+  fahrenheit.classList.remove("selected");
+}
+
+function displayFah() {
+  console.log(temperatureFah);
+  temp.textContent = `${temperatureFah}°F`;
+  celcius.classList.remove("selected");
+  fahrenheit.classList.add("selected");
+}
 
 async function getWeatherFromSearch() {
-  function displayMainWeather(weatherInfo) {
-    console.log(`${weatherInfo.main.humidity}%`);
-    console.log(weatherInfo);
-  }
 
   try {
     if (searchbar.value === "") {
@@ -42,14 +128,22 @@ async function getWeatherFromSearch() {
         { mode: "cors" }
       );
       const refinedData = await refinedSearch.json();
-      displayMainWeather(refinedData);
+      error.style.visibility = "hidden";
+      return displayMainWeather(refinedData);
     }
   } catch {
-    console.log("something fucked up");
-    p.textContent = "something fucked up";
+    error.textContent = "Please enter a valid city name!"
+    error.style.visibility = "visible";
+    return error;
+    // error.style.removeAttribute("visibility");
     // say that it's not a valid city or whatever.
   }
+  return console.log("returned??");
 }
+
+getWeatherFromSearch();
 
 searchbutton.addEventListener("click", getWeatherFromSearch);
 // validate search bar
+celcius.addEventListener("click",displayCel)
+fahrenheit.addEventListener("click", displayFah)
